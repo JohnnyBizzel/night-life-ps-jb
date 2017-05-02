0aeaaad654f0b6fe0d57ac11397f2d1723ffb92f
System <none>
2017-05-02 09:01:37 +0000

    Bug fix Remove RSVP and style changes

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 MERN Stack App allows you to search for local restaurants and alert others if you plan to visit them.
 
-This full stack App was created as part of a challenge for [FreeCodeCamp](http://www,freecodecamp.com)
+This full stack App was created as part of a challenge for [FreeCodeCamp](http://www,freecodecamp.com) and uses the [Yelp.com](http://www.yelp.com) API
+
+**[See the Night life app running](https://night-life-ps-jb.glitch.me/)**
 
 Key Technologies Used:
 
@@ -19,3 +21,5 @@ Key Technologies Used:
 * HTML5 & CSS3
 
 Built by [Parminder Sanghera](https://www.freecodecamp.com/ajitsy) and [John Barber](https://www.freecodecamp.com/johnnybizzel)
+
+
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,8 +3,9 @@ import SearchBar from '../containers/search_bar';
 import YelpList from '../containers/yelp_list';
 
 export default () => <div className="welcome">
-                      <div className="heading text-center">Welcome to the <span className="appname">Nightlife App</span></div> 
-                      <div className='subheading text-center'><i>Search a location for restaurants near you and signup to let others know you plan to visit</i></div>
+                      <div className="heading text-center">The Night Life Co-ordinator App</div> 
+                      <div className='subheading text-center'><i>Search a location for restaurants near you, 
+                        then sign up or log in to let others know you plan to visit</i></div>
                       <SearchBar />
                       <YelpList />                      
                     </div>;
